e6e2858d3e5c92c68275508d1fabc63acae1c683
Nizami Sevindi <nsevindi87@gmail.com>
2022-09-20 00:00:41 +0200

    practice

--- src/components/User.js
+++ src/components/User.js
@@ -15,12 +15,18 @@ class User extends Component {
     const {name,department,salary} = this.props;
 
     return (
-      <div>
-        <ul>
-          <li>Isim: {name} <i className='far fa-trash-alt'></i> </li>
-          <li>Departman: {department}</li>
-          <li>Maas: {salary}</li>
-        </ul>
+      <div className=' col-md-8 mb-4'>
+        <div className="card">
+          <div className="card-header d-flex justify-content-between">
+            <h4 className="d-inline">{name}</h4>
+            <i className='far fa-trash-alt' style={{cursor: "pointer"}}></i>
+          </div>
+          <div className="card-body">
+            <p className="card-text">Departman: {department}</p>
+            <p className="card-text">Maas: {salary}</p>
+          </div>
+        </div>
+        
       </div>
     )
   }
