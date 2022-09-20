c8f6477ac18dd3a1ae8b7fb773b7237ed9eb3f08
Nizami Sevindi <nsevindi87@gmail.com>
2022-09-20 23:27:09 +0200

    setstate

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import PropTypes from 'prop-types'
+import { isVisible } from '@testing-library/user-event/dist/utils';
 
 
 class User extends Component {
@@ -9,22 +10,40 @@ class User extends Component {
     salary : "Bilgi Yok"
   }
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isVisible: false
+    }
+  }
+
+  onClickEvent(e){
+    this.setState ({
+      isVisible: !this.state.isVisible
+    })
+  }
+
   render() {
 
     //Destructing
     const {name,department,salary} = this.props;
+    const {isVisible} = this.state
 
     return (
       <div className=' col-md-8 mb-4'>
         <div className="card">
           <div className="card-header d-flex justify-content-between">
-            <h4 className="d-inline">{name}</h4>
+            <h4 className="d-inline" onClick={this.onClickEvent.bind(this, 34)} >{name}</h4>
             <i className='far fa-trash-alt' style={{cursor: "pointer"}}></i>
           </div>
-          <div className="card-body">
+          {
+            isVisible ? <div className="card-body">
             <p className="card-text">Departman: {department}</p>
             <p className="card-text">Maas: {salary}</p>
-          </div>
+            <p>{this.state.test}</p>
+          </div> : null
+          }
         </div>
         
       </div>
